0601b2533b6440654906564e95f9d8b59cf2d350
Mumin8 <alhassanmumin8@gmail.com>
2023-10-10 23:50:55 +0000

    component to render videos

--- my-youtube-project/src/components/Videos.jsx
+++ my-youtube-project/src/components/Videos.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Stack,  Box } from '@mui/material'
+import { VideoCard, ChannelCard } from './'
 
-function Videos() {
+function Videos({ videos }) {
+  console.log(videos)
+  
   return (
-    <div>Videos</div>
+    <Stack direction='row' 
+    flexWrap='wrap'
+    justifyContent='start' gap={2}
+    >
+      {videos.map((item, idx)=>(
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelCard && <ChannelCard channelDetails={item} />}
+        </Box>
+      ))}
+    </Stack>
   )
 }
 
