9c13d5cd87d79640d91d7c930b8238ec0f8da33b
Mumin8 <alhassanmumin8@gmail.com>
2023-10-10 23:49:53 +0000

    refactor

--- my-youtube-project/src/components/Feed.jsx
+++ my-youtube-project/src/components/Feed.jsx
@@ -2,8 +2,15 @@ import React, {useState, useEffect} from 'react'
  import { Stack, Box,Typography } from '@mui/material'
  import SideBar from './SideBar'
  import Videos from './Videos'
+ import { fetchFromRapidApi } from '../utils/fetchFromRapidApi'
 
 function Feed() {
+  const [selectCat, setSelectCat ] = useState('New')
+  const [videos, setVideos] = useState([])
+  useEffect(() =>{
+    fetchFromRapidApi(`search? part=snippet&q=${selectCat}`)
+    .then((data) =>setVideos(data.items))
+  }, [selectCat])
   return (
     <Stack sx={{ flexDirection: { sx: 'column',
     md: 'row',}}}>
@@ -11,7 +18,8 @@ function Feed() {
 
 <Box sx={{ height: {sx: 'auto', md: '92vh'},
 borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2}}}>
-< SideBar />
+< SideBar selectCat = {selectCat} setSelectCat = {setSelectCat} />
+
 <Typography className='copyright' 
 variant='body2' sx={{mt: 1.5,
 color: '#fff'}}>
@@ -23,10 +31,10 @@ color: '#fff'}}>
   <Typography variant='h4'
   fontWeight='bold' mb={2} sx={{
     color: 'white'
-  }}>New
-    <span style={{color: '#F31503'}}> videos</span>
+  }}>
+    {selectCat}<span style={{color: '#F31503'}}> videos</span>
   </Typography>
-  <Videos />
+  <Videos videos={videos} />
 </Box>
 
     </Stack>
