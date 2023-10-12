75f93c3935331306556e81abfd0997d570bff29d
Mumin8 <alhassanmumin8@gmail.com>
2023-10-12 23:08:07 +0000

    refactor

--- my-youtube-project/src/components/VideoDetail.jsx
+++ my-youtube-project/src/components/VideoDetail.jsx
@@ -3,16 +3,20 @@ import {useParams, Link } from 'react-router-dom'
 import ReactPlayer from 'react-player'
 import {Typography, Box, Stack } from '@mui/material'
 import { CheckCircle } from '@mui/icons-material'
-import { Video } from './'
+import { Videos } from './'
 import { fetchFromRapidApi } from '../utils/fetchFromRapidApi'
 
 function VideoDetail() {
   const [videoDetail, setVideoDetail] = useState(null)
+  const [videos, setVideos] = useState(null)
   const { id } = useParams()
 
   useEffect(()=>{
     fetchFromRapidApi(`videos? part=snippet, statistics&id=${id}`)
     .then((data)=>setVideoDetail(data.items[0]))
+
+    fetchFromRapidApi(`search? part=snippet&relatedToVideoId=${id}&type=video`)
+    .then((data)=>setVideos(data.items))
 
   }, [id])
   if (!videoDetail?.snippet) return 'Loading'
@@ -36,13 +40,30 @@ likeCount}} = videoDetail
           color: 'white'
         }} py={1} px={2}>
           <Link to={`/channel/${channelId}`}>
-            <Typography>
+            <Typography variant={{sm: 'subtitle1', md: 'h6'}}
+            color='white'>
               {channelTitle}
+              <CheckCircle sx={{fontSize: '12px', color: 'gray',
+            ml: '5px'}} />
             </Typography>
           </Link>
+          <Stack direction='row' gap='20px' alignItems='center'>
+            <Typography variant='body1' sx={{opacity: 0.6}}> 
+              {parseInt(viewCount).toLocaleString()} views
+            </Typography>
+            <Typography variant='body1' sx={{opacity: 0.6}}> 
+              {parseInt(likeCount).toLocaleString()} likes
+            </Typography>
+          </Stack>
         </Stack>
         </Box>
       </Box>
+
+      <Box px={2} py={{md: 1, xs: 5}} justifyContent='center'
+        alignItems='center'>
+          <Videos videos={videos} direction='column' />
+        </Box>
+
       </Stack>
     </Box>
   )
