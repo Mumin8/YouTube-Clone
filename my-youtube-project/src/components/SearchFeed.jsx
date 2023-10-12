9e821cd134a5844cb4b06717ad4e400a7b8b85bd
Mumin8 <alhassanmumin8@gmail.com>
2023-10-12 17:32:54 +0000

    makes the search feed functional'

--- my-youtube-project/src/components/SearchFeed.jsx
+++ my-youtube-project/src/components/SearchFeed.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+ import { Box,Typography } from '@mui/material'
+ import Videos from './Videos'
+ import { useParams } from 'react-router-dom'
+ import { fetchFromRapidApi } from '../utils/fetchFromRapidApi'
 
-function SearchTerm() {
+function SearchFeed() {
+ 
+  const [videos, setVideos] = useState([])
+  const { searchTerm } = useParams()
+
+  useEffect(() =>{
+    fetchFromRapidApi(`search? part=snippet&q=${ searchTerm }`)
+    .then((data) =>setVideos(data.items))
+  }, [searchTerm])
   return (
-    <div>SearchTerm</div>
+    <Box p={2} sx={{overflow: 'auto', height: '90vh', flex: 2}}>
+  <Typography variant='h4'
+  fontWeight='bold' mb={2} sx={{
+    color: 'white'
+  }}>
+    Search Result for <span style={{color: '#F31503'}}> { searchTerm }</span> Videos
+  </Typography>
+  <Videos videos={videos} />
+</Box>
   )
 }
 
-export default SearchTerm
+export default SearchFeed
