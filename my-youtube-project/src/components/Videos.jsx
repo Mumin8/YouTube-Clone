73568f357c69aa78d2b26c87abc51ed8ef634fbb
Mumin8 <alhassanmumin8@gmail.com>
2023-10-12 23:08:49 +0000

    refactor

--- my-youtube-project/src/components/Videos.jsx
+++ my-youtube-project/src/components/Videos.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { Stack,  Box } from '@mui/material'
 import { VideoCard, ChannelCard } from './'
 
-function Videos({ videos }) {
+function Videos({ videos, direction }) {
+  if (!videos?.length) return 'Loading ...'
   return (
-    <Stack direction='row' 
+    <Stack direction={direction || 'row'} 
     flexWrap='wrap'
     justifyContent='start' gap={2}
     >
